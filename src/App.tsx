f22ee35cab74be978ad4069203df5402458bc107
Daniel Do <dodaniel006@gmail.com>
2025-08-08 20:49:49 -0700

    Add more fields to the form and state

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import { useState } from 'react'
 import Header from './components/Header.tsx';
 
 function App() {
-  const [posts, setPosts] = useState<string[]>([]);
+  const [posts, setPosts] = useState<{ title: string, companyName: string, applicationDate: string, lastUpdatedDate: string, status: string }[]>([]);
   const [title, setTitle] = useState<string>("");
+  const [companyName, setCompanyName] = useState<string>("");
+  const [applicationDate, setApplicationDate] = useState<string>("");
+  const [status, setStatus] = useState<string>("");
 
   if (posts.length > 0) {
     document.getElementById("postList")?.classList.remove("d-none");
@@ -13,13 +16,24 @@ function App() {
 
     e.preventDefault();
     const form = e.currentTarget;
-    const input = form.elements.namedItem("postTitle") as HTMLInputElement | null;
+    const titleInput = form.elements.namedItem("postTitle") as HTMLInputElement | null;
+    const companyInput = form.elements.namedItem("companyName") as HTMLInputElement | null;
+    const dateInput = form.elements.namedItem("applicationDate") as HTMLInputElement | null;
+    const statusInput = form.elements.namedItem("status") as HTMLSelectElement | null;
 
     if (title !== "") {
-      setPosts((posts) => [...posts, title]);
-      setTitle("");
-      if (input) {
-        input.value = "";
+      setPosts((posts) => [...posts, { title: title, companyName: companyName, applicationDate: applicationDate, lastUpdatedDate: applicationDate, status: status }]);
+      if (titleInput) {
+        titleInput.value = "";
+      }
+      if (companyInput) {
+        companyInput.value = "";
+      }
+      if (dateInput) {
+        dateInput.value = "";
+      }
+      if (statusInput) {
+        statusInput.value = "NA";
       }
     }
   }
@@ -32,8 +46,8 @@ function App() {
         <h1 className="display-4 mb-3">CareerNode</h1>
         <p className="text">Track your job applications and stay organized on your career journey.</p>
 
-        <div className="card shadow-sm mb-3" style={{ maxHeight: '500px' }}>
-          <div className="card-body">
+        <div className="card shadow-sm mb-3" style={{ minHeight: '500px' }}>
+          <div className="card-body d-flex justify-content-center align-items-center flex-column">
 
             {/* Button to open modal */}
             <button
@@ -41,6 +55,7 @@ function App() {
               className="btn btn-primary"
               data-bs-toggle="modal"
               data-bs-target="#exampleModal"
+              style={{ maxWidth: '100px' }}
             >
               Add Post
             </button>
@@ -67,22 +82,59 @@ function App() {
 
                   <div className="modal-body">
 
-                    <form className="mb-3" onSubmit={handleSubmit}>
-                        <input
-                          type="text"
-                          className="form-control mb-3"
-                          placeholder="Enter Job Title"
-                          name="postTitle"
-                          onChange={
-                            (event) => { if (event) { setTitle(event.target.value) } }} //Update the title state
-                        />
+                    <form className="mb-3" autoComplete="off" onSubmit={handleSubmit}>
 
-                        <button
-                          type="submit"
-                          className="btn btn-primary mt-3"
-                        >
-                          Add Post
-                        </button>
+                      <label htmlFor="postTitle">Job Title</label>
+                      <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter Job Title"
+                        name="postTitle"
+                        onChange={(event) => setTitle(event.target.value)}
+                        required
+                      />
+
+                      <label htmlFor="companyName">Company Name</label>
+                      <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter Company Name"
+                        name="companyName"
+                        onChange={(event) => setCompanyName(event.target.value)}
+                        required
+                      />
+
+                      <label htmlFor="applicationDate">Application Date</label>
+                      <input
+                        type="date"
+                        className="form-control mb-3"
+                        placeholder="Enter Application Date"
+                        name="applicationDate"
+                        onChange={(event) => setApplicationDate(event.target.value)}
+                        required
+                      />
+
+                      <label htmlFor="status">Application Status</label>
+                      <select
+                        className="form-select mb-3"
+                        name="status"
+                        onChange={(event) => setStatus(event.target.value)}
+                        defaultValue=""
+                        required
+                      >
+                        <option value="NA">Not Applied</option>
+                        <option value="applied">Applied</option>
+                        <option value="interview">Interview</option>
+                        <option value="offer">Offer</option>
+                        <option value="rejected">Rejected</option>
+                      </select>
+
+                      <button
+                        type="submit"
+                        className="btn btn-primary mt-3"
+                      >
+                        Add Post
+                      </button>
                     </form>
                   </div>
 
@@ -101,8 +153,17 @@ function App() {
 
             <ul id="postList" className="list-unstyled d-none mt-3 mb-1">
               {posts.map((posts, index) => (
-                <li key={index} className="border py-2 mt-2">
-                  {posts}
+                <li key={index} className="card mb-3">
+                  <div className="card-body text-start">
+                    <h5 className="card-title">{posts.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{posts.companyName}</h6>
+                    <p className="card-text mb-1">
+                      <strong>Application Date:</strong> {posts.applicationDate}
+                    </p>
+                    <p className="card-text mb-0">
+                      <strong>Status:</strong> {posts.status}
+                    </p>
+                  </div>
                 </li>
               ))}
             </ul>
